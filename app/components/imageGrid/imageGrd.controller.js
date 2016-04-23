80b33f1b54e46f7bc03ef96da248c96a493e8cc1
Stanislav Kovalev <djently@yandex.ru>
2016-04-23 16:14:46 +0300

    grid image layout

--- app/components/imageGrid/imageGrd.controller.js
+++ app/components/imageGrid/imageGrd.controller.js
@@ -1,17 +1,48 @@
+import $ from 'jquery';
+import debounce from 'lodash.debounce';
+
 class ImageGridController {
-    constructor(ImagesService) {
+    constructor($element, $timeout, ImagesService) {
+        this.$element = $element;
         this.images = [];
 
         ImagesService.getImages(1, 50).then((images) => {
             this.images = this.images.concat(images);
-        })
+            $timeout(() => {
+                this.setGridColumns();
+            }, 0, false);
+        });
     }
 
     removeImage(img) {
         this.images.splice(this.images.indexOf(img), 1);
     }
+
+    $onInit() {
+        $(window).on('resize', 
+            debounce(
+                this.setGridColumns.bind(this), 
+                100, {trailing: true}
+            )
+        );
+    }
+
+    setGridColumns() {
+        var $element = $(this.$element),
+            containerWidth = parseInt(
+                $element.find('.ig-grid-container').innerWidth(), 10
+            ),
+            imageWidth = parseInt(
+                $element.find('.ig-grid-container--image').innerWidth(), 10
+            );
+
+        if (!containerWidth || !imageWidth) return;
+
+        $element.find('.ig-grid-container')
+            .css('column-count', Math.floor(containerWidth / imageWidth));
+    }
 }
 
-ImageGridController.$inject = ['ImagesService'];
+ImageGridController.$inject = ['$element', '$timeout', 'ImagesService'];
 
 export default ImageGridController;
--- app/directives/GridImage/GridImage.directive.js
+++ app/directives/GridImage/GridImage.directive.js
@@ -8,46 +8,5 @@ export default function gridImageDirective() {
 }
 
 function gridImageLink(scope, element, attrs, ctrl) {
-    var width = parseInt(attrs.width, 10),
-        index = $(element).index(),
-        minPadding = 10,
-        containerWidth = $(element).parent().innerWidth(),
-        columns = Math.floor(containerWidth / (width + minPadding * 2)),
-        padding = Math.floor((containerWidth - width * columns) / columns);
 
-    $(element).css({
-        position: 'absolute',
-        left: width * (index % 5) + padding * (index % 5 + 0.5)
-    })
-
-    $(element).on('load', function() {
-        if (index / 5 < 1) { 
-            $(element).css('top', padding / 2) 
-        } else {
-            setElementYPosition(element, index, columns, padding);
-            $(element).parent().find('[grid-image]').eq(index - columns + 1)
-                .one('load', function() {
-                    setElementYPosition(element, index);
-                })
-            $(window).on('resize', function() {
-                setElementYPosition(element, index, columns, padding);
-            })
-        }
-    })
-}
-
-function setElementYPosition(element, index, columns, padding) {
-    var $upperElement = getUpperItem(element, index, columns);
-    console.log(element, $upperElement);
-
-    if ($upperElement.position() && $upperElement.height()) {
-        console.log(index);
-        $(element).css('top', $upperElement.position().top + $upperElement.height() + padding);
-    }
-}
-
-function getUpperItem(element, columns) {
-    var $element = $(element);
-    return $element.parent().children().
-        eq($element.index() - columns);
 }
