b82180a72cd6c48cddc6c029529a87e38d750853
Stanislav Kovalev <djently@yandex.ru>
2016-04-23 20:06:02 +0300

    image loaded animation

--- app/directives/GridImage/GridImage.directive.js
+++ app/directives/GridImage/GridImage.directive.js
@@ -1,12 +1,21 @@
+import './GridImage.css';
 import $ from "jquery";
 
 export default function gridImageDirective() {
     return {
         restrict: 'A',
-        link: gridImageLink
+        link: {
+            pre: gridImageLink
+        }
     }
 }
 
-function gridImageLink(scope, element, attrs, ctrl) {
+function gridImageLink(scope, element) {
+    const $element = $(element);
 
+    $element.addClass('grid-image');
+
+    $element.on('load', () => {
+        $element.addClass('grid-image--loaded');
+    });
 }
